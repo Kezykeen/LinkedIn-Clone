17740d09a9fb0f44474249078641636396eeb6e0
kezyApps <k.okoro@genesystechhub.com>
2021-07-14 15:58:32 +0100

    Implemented header component

---
+++ src/components/header/Header.js
@@ -0,0 +1,41 @@
+import {
+  BusinessCenter,
+  Chat,
+  Notifications,
+  Search,
+  SupervisorAccount,
+} from "@material-ui/icons";
+import React from "react";
+import HeaderOption from "../headerOption/HeaderOption";
+import HomeIcon from "@material-ui/icons/Home";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header_left">
+        <img
+          src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg"
+          alt="linkedIn logo"
+        />
+        <div className="header_search">
+          <Search />
+          <input type="text" />
+        </div>
+      </div>
+      <div className="header_right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccount} title="My Network" />
+        <HeaderOption Icon={BusinessCenter} title="Jobs" />
+        <HeaderOption Icon={Chat} title="Messaging" />
+        <HeaderOption Icon={Notifications} title="Notifications" />
+        <HeaderOption
+          avatar="https:///compassionate-leakey-e9b16b.netlify.app/images/IG_Sonny.jpeg"
+          title="Me"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/headerOption/HeaderOption.js
@@ -0,0 +1,15 @@
+import { Avatar } from "@material-ui/core";
+import React from "react";
+import "./HeaderOption.css";
+
+const HeaderOption = ({ avatar, Icon, title }) => {
+  return (
+    <div className="headerOption">
+      {Icon && <Icon className="headerOption_icon" />}
+      {avatar && <Avatar className="headerOption_icon" src={avatar} />}
+      <h3 className="headerOption_title">{title}</h3>
+    </div>
+  );
+};
+
+export default HeaderOption;
