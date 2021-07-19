ab7283479f1a98422aac7146372432e83be19835
kezyApps <k.okoro@genesystechhub.com>
2021-07-19 11:23:28 +0100

    Added the post component

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,37 @@
+import { Avatar } from "@material-ui/core";
+import {
+  ChatOutlined,
+  SendOutlined,
+  ShareOutlined,
+  ThumbUpAltOutlined,
+} from "@material-ui/icons";
+import React from "react";
+import InputOption from "../inputOption/InputOption";
+import "./Post.css";
+
+const Post = ({ name, description, message, photoUrl }) => {
+  return (
+    <div className="post">
+      <div className="post_header">
+        <Avatar />
+        <div className="post_info">
+          <h2>{name}</h2>
+          <p>{description}</p>
+        </div>
+      </div>
+
+      <div className="post_body">
+        <p>{message}</p>
+      </div>
+
+      <div className="post_buttons">
+        <InputOption Icon={ThumbUpAltOutlined} title="Like" color="gray" />
+        <InputOption Icon={ChatOutlined} title="Comment" color="gray" />
+        <InputOption Icon={ShareOutlined} title="Share" color="gray" />
+        <InputOption Icon={SendOutlined} title="Send" color="gray" />
+      </div>
+    </div>
+  );
+};
+
+export default Post;
