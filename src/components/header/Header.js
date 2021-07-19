6ce1515acda991ab94b7c47bf665c902b7055d4a
kezyApps <k.okoro@genesystechhub.com>
2021-07-19 11:18:12 +0100

    Fixed the width on the header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -13,26 +13,28 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="header">
-      <div className="header_left">
-        <img
-          src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg"
-          alt="linkedIn logo"
-        />
-        <div className="header_search">
-          <Search />
-          <input type="text" />
+      <div className="header_container">
+        <div className="header_left">
+          <img
+            src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg"
+            alt="linkedIn logo"
+          />
+          <div className="header_search">
+            <Search />
+            <input type="text" />
+          </div>
         </div>
-      </div>
-      <div className="header_right">
-        <HeaderOption Icon={HomeIcon} title="Home" />
-        <HeaderOption Icon={SupervisorAccount} title="My Network" />
-        <HeaderOption Icon={BusinessCenter} title="Jobs" />
-        <HeaderOption Icon={Chat} title="Messaging" />
-        <HeaderOption Icon={Notifications} title="Notifications" />
-        <HeaderOption
-          avatar="https:///compassionate-leakey-e9b16b.netlify.app/images/IG_Sonny.jpeg"
-          title="Me"
-        />
+        <div className="header_right">
+          <HeaderOption Icon={HomeIcon} title="Home" />
+          <HeaderOption Icon={SupervisorAccount} title="My Network" />
+          <HeaderOption Icon={BusinessCenter} title="Jobs" />
+          <HeaderOption Icon={Chat} title="Messaging" />
+          <HeaderOption Icon={Notifications} title="Notifications" />
+          <HeaderOption
+            avatar="https:///compassionate-leakey-e9b16b.netlify.app/images/IG_Sonny.jpeg"
+            title="Me"
+          />
+        </div>
       </div>
     </div>
   );
