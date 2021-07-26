3a4b8a827a2d11a1dace415cc0eb2a806916d6bd
kezykeen <okorokingsley250@gmail.com>
2021-07-26 08:31:04 +0100

    Implemented logout functionality

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -9,8 +9,18 @@ import React from "react";
 import HeaderOption from "../headerOption/HeaderOption";
 import HomeIcon from "@material-ui/icons/Home";
 import "./Header.css";
+import { useDispatch } from "react-redux";
+import { logout } from "../../features/userSlice";
+import { auth } from "../../firebase";
 
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const logoutOfApp = () => {
+    dispatch(logout());
+    auth.signOut();
+  };
+
   return (
     <div className="header">
       <div className="header_container">
@@ -21,19 +31,16 @@ const Header = () => {
           />
           <div className="header_search">
             <Search />
-            <input type="text" />
+            <input type="text" placeholder="Search" />
           </div>
         </div>
         <div className="header_right">
-          <HeaderOption Icon={HomeIcon} title="Home" />
+          <HeaderOption Icon={HomeIcon} title="Home" onClick={logoutOfApp} />
           <HeaderOption Icon={SupervisorAccount} title="My Network" />
           <HeaderOption Icon={BusinessCenter} title="Jobs" />
           <HeaderOption Icon={Chat} title="Messaging" />
           <HeaderOption Icon={Notifications} title="Notifications" />
-          <HeaderOption
-            avatar="https:///compassionate-leakey-e9b16b.netlify.app/images/IG_Sonny.jpeg"
-            title="Me"
-          />
+          <HeaderOption avatar title="Me" handleClick={logoutOfApp} />
         </div>
       </div>
     </div>
