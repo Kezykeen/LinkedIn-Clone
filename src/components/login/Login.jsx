e83f2df2cfe740f1311ea1e89ff27c805aec4d97
kezykeen <okorokingsley250@gmail.com>
2021-07-26 08:37:43 +0100

    Added login functionality

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../../features/userSlice";
+import { auth } from "../../firebase";
+import "./Login.css";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [profilePic, setProfilePic] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+
+  const register = () => {
+    if (!name) {
+      return alert("Please enter a full name");
+    }
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userAuth) => {
+        userAuth.user
+          .updateProfile({
+            displayName: name,
+            photoURL: profilePic,
+          })
+          .then(() => {
+            dispatch(
+              login({
+                email: userAuth.user.email,
+                uid: userAuth.user.uid,
+                displayName: name,
+                photoURL: profilePic,
+              })
+            );
+          });
+      })
+      .catch((error) => alert(error));
+  };
+
+  const LoginToApp = (e) => {
+    e.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((userAuth) => {
+        dispatch(
+          login({
+            email: userAuth.user.email,
+            uid: userAuth.user.uid,
+            displayName: name,
+            photoURL: profilePic,
+          })
+        );
+      })
+      .catch((error) => alert(error));
+  };
+
+  return (
+    <div className="login">
+      <img
+        src="https://news.hitb.org/sites/default/files/styles/large/public/field/image/500px-LinkedIn_Logo.svg__1.png?itok=q_lR0Vks"
+        alt="LinkedIn Logo"
+      />
+      <form>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Full name"
+          required
+        />
+        <input
+          type="text"
+          value={profilePic}
+          onChange={(e) => setProfilePic(e.target.value)}
+          placeholder="Profile pic"
+        />
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          required
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          required
+        />
+        <button type="submit" onClick={LoginToApp}>
+          Sign In
+        </button>
+      </form>
+
+      <p>
+        Not a member?{" "}
+        <span className="login_register" onClick={register}>
+          Register now
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default Login;
